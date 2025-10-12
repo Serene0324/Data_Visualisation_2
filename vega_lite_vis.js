c9d244bcbace11f70c1675dcd295e6e6110181f9
Serene0324 <stan0256@student.monash.edu>
2025-10-12 21:18:44 +0800

    finish constructing the diverging bar chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -7,3 +7,13 @@ var vg_2 = "education_stackedbar.vg.json";
 vegaEmbed("#stacked_bar_chart", vg_2).then(function(result) {
     
 }).catch(console.error);
+
+var vg_3 = "bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_3).then(function(result) {
+    
+}).catch(console.error);
+
+var vg_4 = "gender_divergingbar.vg.json";
+vegaEmbed("#diverging_bar_chart", vg_4).then(function(result) {
+    
+}).catch(console.error);
