0333b7872b1e5f07f0fea061201f9c9cc63ac20c
Serene0324 <stan0256@student.monash.edu>
2025-10-15 03:12:11 +0800

    Arranging the dashboard design

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,19 +1,19 @@
 var vg_1 = "population_choropleth.vg.json";
-vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+vegaEmbed("#choropleth_map", vg_1, { actions: false }).then(function(result) {
 
 }).catch(console.error);
 
 var vg_2 = "education_stackedbar.vg.json";
-vegaEmbed("#stacked_bar_chart", vg_2).then(function(result) {
+vegaEmbed("#stacked_bar_chart", vg_2, { actions: false }).then(function(result) {
     
 }).catch(console.error);
 
 var vg_3 = "bubble_plot.vg.json";
-vegaEmbed("#bubble_plot", vg_3).then(function(result) {
+vegaEmbed("#bubble_plot", vg_3, { actions: false }).then(function(result) {
     
 }).catch(console.error);
 
 var vg_4 = "gender_divergingbar.vg.json";
-vegaEmbed("#diverging_bar_chart", vg_4).then(function(result) {
+vegaEmbed("#diverging_bar_chart", vg_4, { actions: false }).then(function(result) {
     
 }).catch(console.error);
